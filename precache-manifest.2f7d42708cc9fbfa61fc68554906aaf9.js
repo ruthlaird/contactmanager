42d2ee715898d7251cb7befbd38347c86d084c1e
Ruth Laird <ruth.laird@anaplan.com>
2019-09-24 14:51:14 +0100

    Updates

---
+++ precache-manifest.2f7d42708cc9fbfa61fc68554906aaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.a9b80a0b.js"
+  },
+  {
+    "revision": "4445d4bf45951c0f146a",
+    "url": "/contactmanager/static/js/main.4445d4bf.chunk.js"
+  },
+  {
+    "revision": "a7942763c056a5be8692",
+    "url": "/contactmanager/static/js/2.a7942763.chunk.js"
+  },
+  {
+    "revision": "4445d4bf45951c0f146a",
+    "url": "/contactmanager/static/css/main.7b02a67c.chunk.css"
+  },
+  {
+    "revision": "a7942763c056a5be8692",
+    "url": "/contactmanager/static/css/2.5744dfe0.chunk.css"
+  },
+  {
+    "revision": "021fb6402194f582c3349286d218ee29",
+    "url": "/contactmanager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ruthlaird/contactmanager/precache-manifest.c0e9976e9508f712e979124e429037f0.js"
+  "/contactmanager/precache-manifest.2f7d42708cc9fbfa61fc68554906aaf9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ruthlaird/contactmanager/index.html", {
+workbox.routing.registerNavigationRoute("/contactmanager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
